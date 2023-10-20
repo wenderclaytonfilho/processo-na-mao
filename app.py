2a8bbcf75232d78c4fc25fe274b8e24ff138d09b
wenderclaytonfilho <rednew2727@gmail.com>
2023-10-20 11:44:03 -0300

    funcinalidade de enviar email finalizada

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import *
+from util import *
 
 app = Flask(__name__)
-
-@app.route("/", methods=[GET,POST])
-def index(name=None):
+codigoatual = 0
+@app.route("/", methods=['GET','POST'])
+def buscar_processo(name=None):
   if request.method == 'POST':
-    pass
+    codigo = gerar_codigo()
+    email = request.form.get('email')
+    enviar_email(email,codigo)
+    return redirect(url_for('confirmar'))
   return render_template('index.html',name=name)
 
-@app.route("/about")
-#About
-def about(name=None):
+@app.route("/confirmar", methods=['GET','POST'])
+def confirmar(name='confirmar'):
+  #print(request)
+  #if request.method=='POST' or request.method=='GET':
+    #codigodeconfirmacao = request.form.get('codigodeconfirmacao')
+    #print(codigodeconfirmacao)
+    #if verificar_codigo(codigoatual,codigodeconfirmacao):
+    ##print("Códigos errados")
+
   return render_template('confirm.html',name=name)
 
 
--- util.py
+++ util.py
@@ -1,16 +1,17 @@
 import smtplib
 import email.message
+import random
 
-def enviar_email():  
+def enviar_email(mail,codigo):  
     corpo_email = """
     <p>Olá, segue o código de verificação</p>
-    <p>Código</p>
-    """
+    <p>{}</p>
+    """.format(codigo)
 
     msg = email.message.Message()
     msg['Subject'] = "Assunto"
     msg['From'] = 'processonamao@gmail.com' #processonamao2802
-    msg['To'] = 'wenderifpe@gmail.com'
+    msg['To'] = mail
     password = 'ukxmzkwvdokdbkiw' 
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email )
@@ -22,7 +23,12 @@ def enviar_email():
     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
     print('Email enviado')
 
+def gerar_codigo():
+    codigo = random.randint(0,9999)
+    return codigo
 
-
-
-enviar_email()
+def verificar_codigo(codigo,codigorecebido):
+    if codigo == codigorecebido:
+        return True
+    else:
+        return False
